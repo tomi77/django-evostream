13211fd51710bfce2cb5de61443da3b2aae88d8f
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-02-17 11:32:47 +0100

    fix documentation build

--- evostream/__init__.py
+++ evostream/__init__.py
@@ -1,2 +1,9 @@
+__author__ = "Tomasz Jakub Rup"
+__license__ = "MIT"
+__version__ = '0.1.0beta1'
+__email__ = "tomasz.rup@gmail.com"
+__status__ = "Beta"
+
+
 class EvoStreamException(Exception):
     pass
