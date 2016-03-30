353ca2c21050a26829bcd0a591c77c5154111668
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-03-30 20:38:55 +0200

    refactoring tests

--- evostream/tests.py
+++ evostream/tests.py
@@ -27,102 +27,77 @@ def load_test_data(filename):
 
 
 LIST_STREAMS_IDS_TEST_DATA = load_test_data('list_streams_ids.json')
+GET_STREAM_INFO_TEST_DATA = load_test_data('get_stream_info.json')
+LIST_STREAMS_TEST_DATA = load_test_data('list_streams.json')
+GET_STREAMS_COUNT_TEST_DATA = load_test_data('get_streams_count.json')
 
 
 @patch('evostream.commands.logger', Mock())
-@patch('evostream.commands.protocol', TestHTTPProtocol(LIST_STREAMS_IDS_TEST_DATA))
-class ListStreamsIdsTestCase(TestCase):
-    def test_success(self):
-        ids = list_streams_ids()
-        self.assertListEqual(ids, LIST_STREAMS_IDS_TEST_DATA['data'])
+class ApiTestCase(TestCase):
+    @patch('evostream.commands.protocol', TestHTTPProtocol(LIST_STREAMS_IDS_TEST_DATA))
+    def test_list_streams_ids(self):
+        out = list_streams_ids()
+        self.assertListEqual(out, LIST_STREAMS_IDS_TEST_DATA['data'])
+
+    @patch('evostream.commands.protocol', TestHTTPProtocol(GET_STREAM_INFO_TEST_DATA))
+    def test_get_stream_info(self):
+        out = get_stream_info(id=1)
+        self.assertDictEqual(out, GET_STREAM_INFO_TEST_DATA['data'])
+
+    @patch('evostream.commands.protocol', TestHTTPProtocol(LIST_STREAMS_TEST_DATA))
+    def test_list_streams(self):
+        out = list_streams()
+        self.assertListEqual(out, LIST_STREAMS_TEST_DATA['data'])
+
+    @patch('evostream.commands.protocol', TestHTTPProtocol(GET_STREAMS_COUNT_TEST_DATA))
+    def test_get_streams_count(self):
+        out = get_streams_count()
+        self.assertDictEqual(out, GET_STREAMS_COUNT_TEST_DATA['data'])
 
 
 if django.VERSION >= (1, 5):
     @patch('evostream.commands.logger', Mock())
-    @patch('evostream.commands.protocol', TestHTTPProtocol(LIST_STREAMS_IDS_TEST_DATA))
     @patch('django.core.management.base.OutputWrapper.write')
-    class ListStreamsIdsCommandTestCase(TestCase):
-        def test_verbose(self, mock_write):
+    class CliTestCase(TestCase):
+        @patch('evostream.commands.protocol', TestHTTPProtocol(LIST_STREAMS_IDS_TEST_DATA))
+        def test_liststreamsids(self, mock_write):
             call_command('liststreamsids')
             self.assertEqual(mock_write.call_count, 1)
             self.assertEqual(mock_write.call_args,
                              [(json.dumps(LIST_STREAMS_IDS_TEST_DATA['data'], indent=1, sort_keys=True),)])
 
-GET_STREAM_INFO_TEST_DATA = load_test_data('get_stream_info.json')
-
-
-@patch('evostream.commands.logger', Mock())
-@patch('evostream.commands.protocol', TestHTTPProtocol(GET_STREAM_INFO_TEST_DATA))
-class GetStreamInfoTestCase(TestCase):
-    def test_success(self):
-        ids = get_stream_info(id=1)
-        self.assertDictEqual(ids, GET_STREAM_INFO_TEST_DATA['data'])
-
-
-if django.VERSION >= (1, 5):
-    @patch('evostream.commands.logger', Mock())
-    @patch('evostream.commands.protocol', TestHTTPProtocol(GET_STREAM_INFO_TEST_DATA))
-    @patch('django.core.management.base.OutputWrapper.write')
-    class GetStreamInfoCommandTestCase(TestCase):
-        def test_verbose(self, mock_write):
+        @patch('evostream.commands.protocol', TestHTTPProtocol(GET_STREAM_INFO_TEST_DATA))
+        def test_getstreaminfo_verbose(self, mock_write):
             call_command('getstreaminfo', id_or_local_stream_name=1, verbosity=2)
             self.assertEqual(mock_write.call_count, 1)
             self.assertEqual(mock_write.call_args,
                              [(json.dumps(GET_STREAM_INFO_TEST_DATA['data'], indent=1, sort_keys=True),)])
 
-        def test_silent(self, mock_write):
+        @patch('evostream.commands.protocol', TestHTTPProtocol(GET_STREAM_INFO_TEST_DATA))
+        def test_getstreaminfo(self, mock_write):
             call_command('getstreaminfo', id_or_local_stream_name=1)
             self.assertEqual(mock_write.call_count, 2)
             self.assertListEqual(mock_write.call_args_list,
                                  [[('uniqueId: 1\n',)],
                                   [('name: "testpullstream"\n',)]])
 
-LIST_STREAMS_TEST_DATA = load_test_data('list_streams.json')
-
-
-@patch('evostream.commands.logger', Mock())
-@patch('evostream.commands.protocol', TestHTTPProtocol(LIST_STREAMS_TEST_DATA))
-class ListStreamsTestCase(TestCase):
-    def test_success(self):
-        ids = list_streams()
-        self.assertListEqual(ids, LIST_STREAMS_TEST_DATA['data'])
-
-
-if django.VERSION >= (1, 5):
-    @patch('evostream.commands.logger', Mock())
-    @patch('evostream.commands.protocol', TestHTTPProtocol(LIST_STREAMS_TEST_DATA))
-    @patch('django.core.management.base.OutputWrapper.write')
-    class ListStreamsCommandTestCase(TestCase):
-        def test_verbose(self, mock_write):
+        @patch('evostream.commands.protocol', TestHTTPProtocol(LIST_STREAMS_TEST_DATA))
+        def test_liststreams_verbose(self, mock_write):
             call_command('liststreams', verbosity=2)
             self.assertEqual(mock_write.call_count, 1)
             self.assertEqual(mock_write.call_args,
                              [(json.dumps(LIST_STREAMS_TEST_DATA['data'], indent=1, sort_keys=True),)])
 
-        def test_silent(self, mock_write):
+        @patch('evostream.commands.protocol', TestHTTPProtocol(LIST_STREAMS_TEST_DATA))
+        def test_liststreams(self, mock_write):
             call_command('liststreams')
             self.assertEqual(mock_write.call_count, 2)
             self.assertListEqual(mock_write.call_args_list,
                                  [[('uniqueId: 36\n',)],
                                   [('name: "testpullstream"\n',)]])
 
-GET_STREAMS_COUNT_TEST_DATA = load_test_data('get_streams_count.json')
-
-
-@patch('evostream.commands.logger', Mock())
-@patch('evostream.commands.protocol', TestHTTPProtocol(GET_STREAMS_COUNT_TEST_DATA))
-class GetStreamsCountTestCase(TestCase):
-    def test_success(self):
-        cnt = get_streams_count()
-        self.assertDictEqual(cnt, GET_STREAMS_COUNT_TEST_DATA['data'])
-
-
-if django.VERSION >= (1, 5):
-    @patch('evostream.commands.logger', Mock())
-    @patch('evostream.commands.protocol', TestHTTPProtocol(GET_STREAMS_COUNT_TEST_DATA))
-    @patch('django.core.management.base.OutputWrapper.write')
-    class GetStreamsCountCommandTestCase(TestCase):
-        def test_verbose(self, mock_write):
+        @patch('evostream.commands.protocol', TestHTTPProtocol(GET_STREAMS_COUNT_TEST_DATA))
+        def test_getstreamscount(self, mock_write):
             call_command('getstreamscount')
             self.assertEqual(mock_write.call_count, 1)
             self.assertEqual(mock_write.call_args,
